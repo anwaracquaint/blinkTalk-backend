1c7c11f158dae8c7387eaaf34901962b9d59ddee
Anwar Shaikh <anwar.shaikh@acquaintsoft.com>
2023-12-06 14:48:13 +0530

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello world!");
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+import { getDb } from "./db/config.js";
+import userRouter from "./routes/user.router.js";
+
+
+getDb();
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+
+app.use("/user", userRouter);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
---
+++ routes/user.router.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+
+
+
+
+
+
+
+
+export default userRouter;
